3aa2dca4da6950e102f4982c2ecbc1384a85f607
Adebayo Ayomide <adebayormide@gmail.com>
2023-09-20 19:54:32 -0400

    Removed uneeded files

--- Module/lib.js
+++
@@ -1,6 +0,0 @@
-export function half(num) {
-    return num / 2;
-}
-export function multiply(num, num2) {
-    return num * num2;
-}
--- Module/main.js
+++
@@ -1,4 +0,0 @@
-import { half, multiply } from "./lib.js";
-
-console.log(half(4), "half of");
-console.log(multiply(3, 2), "multiply");
